d352094485925b18b3bffc0adcfc35f446528add
SteRobWms <stephen.r.williams.87@gmail.com>
2020-02-14 08:23:57 -0600

    Made journal entries required

--- lib/functions/create_journal.rb
+++ lib/functions/create_journal.rb
@@ -5,7 +5,7 @@ require_relative 'global'
 #lecture_id = lecture_id
 #date_created = 
 def create_entry
-    JournalEntry.create(lecture_id: $current_lecture_id, student_id: $current_student_id, what_did_i_learn: $prompt.ask("What did you learn?"), what_was_unclear: $prompt.ask("What was not clear?"), confidence_level: $prompt.select('How confident are you in the material?', [1, 2, 3, 4, 5]), feelings_on_topic: $prompt.ask("How did you feel on the topic discussed?"), feelings_general: $prompt.ask("How do you personally feel today?"), mood: $prompt.select("What is your mood?", [1, 2, 3, 4, 5]))
+    JournalEntry.create(lecture_id: $current_lecture_id, student_id: $current_student_id, what_did_i_learn: $prompt.ask("What did you learn?", required: true), what_was_unclear: $prompt.ask("What was not clear?", required: true), confidence_level: $prompt.select('How confident are you in the material?', [1, 2, 3, 4, 5], required: true), feelings_on_topic: $prompt.ask("How did you feel about the lecture content?", required: true), feelings_general: $prompt.ask("How do you personally feel today?", required: true), mood: $prompt.select("Rate your mood (5-best, 1-worst", [1, 2, 3, 4, 5], required: true))
 
     $current_journal_id = JournalEntry.last.id
     display_journal_entry($current_journal_id)
